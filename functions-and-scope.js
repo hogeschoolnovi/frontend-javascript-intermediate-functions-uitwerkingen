e749e45587a4621439de27bbd05f29c8455154d8
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-04-07 12:32:14 +0200

    Add solutions

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -16,6 +16,18 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // ---- Verwachte uitkomst: 6
 
+// teller buiten de scope van de for-loop
+let cumLaudeCount = 0;
+
+for (let i = 0; i < grades.length; i++) {
+  if (grades[i] >= 8) {
+    cumLaudeCount = cumLaudeCount + 1;
+    // dit mag ook: cumLaudeCount += 1;
+    // dit mag ook: cumLaudeCount++;
+  }
+}
+
+console.log(cumLaudeCount);
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
@@ -27,7 +39,25 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
+function cumLaude(gradesArray) {
+  let count = 0;
 
+  for (let i = 0; i < gradesArray.length; i++) {
+    if (gradesArray[i] >= 8) {
+      count = count + 1;
+      // dit mag ook: cumLaude += 1;
+      // dit mag ook: cumLaude++;
+    }
+  }
+
+  return count;
+}
+
+const diplomasOne = cumLaude(grades);
+const diplomasTwo = cumLaude([6, 4, 5]);
+const diplomasThree = cumLaude([8, 9, 4, 6, 10]);
+
+console.log(diplomasOne, diplomasTwo, diplomasThree);
 
 
 /* Opdracht  2: Gemiddeld cijfer */
@@ -42,6 +72,14 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // ---- Verwachte uitkomst: 6.642857142857143
 
+let total = 0;
+
+for (let i = 0; i < grades.length; i++) {
+  // tel telkens de waarde van de entry uit de array bij wat we daarvoor al hebben opgetelt
+  total = total + grades[i];
+}
+
+console.log(total / grades.length);
 
 /* 2b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd averageGrade, die een array van cijfers verwacht (zoals grades) en het gemiddelde cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 2a.
@@ -50,15 +88,32 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // ---- Verwachte uitkomsten:
 // averageGrade(grades) geeft 6.642857142857143
-// averageGrade([6, 4, 5]) geeft xxxx
-// averageGrade([8, 9, 4, 6, 10]) geeft xxxx
+// averageGrade([6, 4, 5]) geeft 5
+// averageGrade([8, 9, 4, 6, 10]) geeft 7.4
 
+function averageGrade(gradesArray) {
+  let total = 0;
+
+  for (let i = 0; i < gradesArray.length; i++) {
+    // tel telkens de waarde van de entry uit de array bij wat we daarvoor al hebben opgetelt
+    total = total + gradesArray[i];
+  }
+
+  return total / gradesArray.length;
+}
+
+const averageOne = averageGrade(grades);
+const averageTwo = averageGrade([6, 4, 5]);
+const averageThree = averageGrade([8, 9, 4, 6, 10]);
+
+console.log(averageOne, averageTwo, averageThree);
 
 /* 2c: Afronden op twee decimalen */
 // Zorg ervoor dat het gemiddelde cijfer dat wordt teruggegeven uit de functie netjes wordt afgerond op twee decimalen.
 // Tip: Google is your best friend!
 
-
+// dit kan bereikt worden door de functie .toFixed(2) op het eindresultaat (in de functie!) te gebruiken. Voorbeeld:
+console.log(averageOne.toFixed(2));
 
 
 /* Bonusopdracht: hoogste cijfer */
@@ -72,6 +127,17 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // ---- Verwachte uitkomst: 9
 
+let highest = 0;
+
+for (let i = 0; i < grades.length; i++) {
+  // als de huidige waarde van grades (grades[i]) hoger is dan mijn huidige opgeslagen waarde
+  // sla dan dat nieuwe cijfer op als hoogste waarde!
+  if (grades[i] > highest) {
+    highest = grades[i];
+  }
+}
+
+console.log(highest);
 
 /* 3b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd highestGrade, die een array van cijfers verwacht (zoals grades) en het hoogste cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 3a.
@@ -82,3 +148,23 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // highestGrade(grades) geeft 9
 // highestGrade([6, 4, 5]) geeft 6
 // highestGrade([8, 9, 4, 6, 10]) geeft 10
+
+function highestGrade(gradesArray) {
+  let highest = 0;
+
+  for (let i = 0; i < gradesArray.length; i++) {
+    // als de huidige waarde van grades (grades[i]) hoger is dan mijn huidige opgeslagen waarde
+    // sla dan dat nieuwe cijfer op als hoogste waarde!
+    if (gradesArray[i] > highest) {
+      highest = gradesArray[i];
+    }
+  }
+
+  return highest;
+}
+
+const highestOne = highestGrade(grades);
+const highestTwo = highestGrade([6, 4, 5]);
+const highestThree = highestGrade([8, 9, 4, 6, 10]);
+
+console.log(highestOne, highestTwo, highestThree);
--- functions-methods.js
+++ functions-methods.js
@@ -9,19 +9,60 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
+function getEmailDomain(emailaddress) {
+  // bepaal op welk indexnummer het apenstaartje staat
+  const indexOfApenstaartje = emailaddress.indexOf('@');
+  // knip alles ná het apenstaartje van het emailadres af
+  const domain = emailaddress.substring(indexOfApenstaartje + 1);
 
+  return domain;
+}
 
+const domainOne = getEmailDomain("n.eeken@novi-education.nl");
+const domainTwo = getEmailDomain("t.mellink@novi.nl");
+const domainThree = getEmailDomain("a.wiersma@outlook.com");
+
+console.log(domainOne, domainTwo, domainThree);
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
 // ---- Verwachte uitkomsten:
 // typeOfEmail("n.eeken@novi-education.nl") geeft "Student"
-// typeOfEmail("t.mellink@novi.nçl") geeft geeft "Medewerker"
+// typeOfEmail("t.mellink@novi.nl") geeft geeft "Medewerker"
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+function typeOfEmail(emailaddress) {
+  // je kunt óf de code uit de vorige vraag kopieren, of de functie getEmailDomain hier aanroepen!
+  const indexOfApenstaartje = emailaddress.indexOf('@');
+  const domain = emailaddress.substring(indexOfApenstaartje + 1);
+  console.log(domain);
 
+  // optie 1: switch statement met returns
+  switch(domain) {
+    case "novi-education.nl":
+      return "Student";
+    case "novi.nl":
+      return "Medewerker";
+    default:
+      return "Extern";
+  }
 
+  // optie 2: if-else statement
+  // if (domain === "novi-education.nl") {
+  //   return "Student";
+  // } else if (domain === "novi.nl") {
+  //   return "Medewerker";
+  // } else {
+  //   return "Extern";
+  // }
+}
+
+const typeOne = typeOfEmail("n.eeken@novi-education.nl");
+const typeTwo = typeOfEmail("t.mellink@novi.nl");
+const typeThree = typeOfEmail("novi.nlaapjesk@outlook.com");
+
+console.log(typeOne, typeTwo, typeThree);
 
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
@@ -35,3 +76,28 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity(emailaddress) {
+  const containsApenstaartje = emailaddress.includes('@');
+  const containsComma = emailaddress.includes(',');
+
+  const indexOfLastDot = emailaddress.lastIndexOf('.');
+  const containsNoDotAtEnd = indexOfLastDot !== emailaddress.length - 1;
+
+  if (containsApenstaartje && !containsComma && containsNoDotAtEnd) {
+    return true;
+  } else {
+    return false;
+  }
+
+  // kan nog korter, want de uitkomst van de if-statement is opzichzelf al een true of false waarde!
+  // return containsApenstaartje && !containsComma && containsNoDotAtEnd;
+}
+
+const validOne = checkEmailValidity("n.eeken@novi.nl");
+const validTwo = checkEmailValidity("tessmellink@novi.nl");
+const validThree = checkEmailValidity("n.eekenanovi.nl");
+const validFour = checkEmailValidity("n.eeken@novinl.");
+const validFive = checkEmailValidity("tessmellink@novi,nl");
+
+console.log(validOne, validTwo, validThree, validFour, validFive);
